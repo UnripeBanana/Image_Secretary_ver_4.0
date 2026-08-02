856e3f30058b8375fffa549707f20fdb77905882
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-08-02 14:58:20 +0900

    Update domestic_stock_day_candle_chart_maker.py

--- collector/chart_maker/domestic_stock_day_candle_chart_maker.py
+++ collector/chart_maker/domestic_stock_day_candle_chart_maker.py
@@ -170,8 +170,8 @@ def domestic_stock_day_candle_chart_maker(stock, name):
     tick_labels = [stock.loc[i, "date"].strftime(date_format) for i in tick_positions]
 
     # 4. 축 적용
-    ax_l.set_xticks(tick_positions)
-    ax_l.set_xticklabels(
+    ax.set_xticks(tick_positions)
+    ax.set_xticklabels(
         tick_labels,
         rotation=0,
         fontsize=9,
